88bb42bc4af3dcef1828d691367f2da426753eb2
Lars van der Bijl <lars@realisestudio.com>
2012-02-20 14:45:51 +0000

    update test cases.

--- test_homport.py
+++ test_homport.py
@@ -1,47 +1,48 @@
 import unittest
 import homport
-homport.bootstrap()
 
 class NodeWrapTestCase(unittest.TestCase):
-    def setUp(self):
-        self.assertTrue('hou' in globals())
 
-    def testWrapped(self):
-        self.assertTrue(isinstance(hou.node('/obj'), homport.NodeWrap))
+    def setUp(self):
+        hou.hipFile.clear(suppress_save_prompt = True)
 
     def testGetNode(self):
         hou.node('/obj').createNode('geo')
-        self.assertEquals(hou.node('/obj/geo1').node, hou.node('/obj').geo1.node)
+        self.assertEquals(hou.node('/obj/geo1'), hou.node('/obj').geo1)
+
+    def testGetParent(self):
+        node = hou.node('/obj').createNode('geo')
+        self.assertEquals(hou.node('/obj'), -node)
 
     def testGetParm(self):
         null = hou.node('/obj').createNode('null')
-        self.assertEquals(null.tx.parm, null.parm('tx'))
+        self.assertEquals(null.tx, null.parm('tx'))
 
     def testRshift(self):
         geo = hou.node('/obj').createNode('geo')
         null = hou.node('/obj').createNode('null')
         geo >> null
+        self.assertEquals(null.inputs()[0].name(), geo.name())
 
     def testLshift(self):
         geo = hou.node('/obj').createNode('geo')
         null = hou.node('/obj').createNode('null')
         geo << null
+        self.assertEquals(geo.inputs()[0].name(), null.name())
 
     def testFloorDiv(self):
         geo = hou.node('/obj').createNode('geo')
         null = hou.node('/obj').createNode('null')
         geo >> null
         self.assertTrue(len(null.inputConnections()) == 1)
-
         geo // null
         self.assertTrue(len(null.inputConnections()) == 0)
 
     def testDefinedInputConn(self):
         geo = hou.node('/obj').createNode('geo')
         subnet = hou.node('/obj').createNode('subnet')
-        geo >> subnet.input_two
-        self.assert_(subnet.inputConnectors()[1])
-
+        geo >> subnet[1]
+        self.assertTrue(subnet.inputConnectors()[1])
 
 class TestParmTupleWrap(object):
 
@@ -50,24 +51,24 @@ class TestParmTupleWrap(object):
         self.geo2 = hou.node('/obj').createNode('geo')
 
     def testGetParent(self):
-        assert_equal(-self.geo1.t, self.geo1)
+        self.assertEquals(-self.geo1.t, self.geo1)
 
     def testSetParm(self):
         self.geo1.t = (500, 0, 0)
-        assert_equal(self.geo1.t.eval(), (500.0, 0.0, 0.0))
+        self.assertEquals(self.geo1.t.eval(), (500.0, 0.0, 0.0))
 
     def testEvalParm(self):
         self.geo1.t.set((500, 0, 0))
-        assert_equal(self.geo1.t.eval(), (500.0, 0.0, 0.0))
+        self.assertEquals(self.geo1.t.eval(), (500.0, 0.0, 0.0))
 
     def testStrParm(self):
         self.geo1.t.set((500, 0, 0))
-        assert_equal(str(self.geo1.t), str((500.0, 0.0, 0.0)))
+        self.assertEquals(str(self.geo1.t), str((500.0, 0.0, 0.0)))
 
     def testLinkParms(self):
         self.geo1.t >> self.geo2.t
         self.geo1.t = (500, 0, 0)
-        assert_equal(str(self.geo1.t), str(self.geo2.t))
+        self.assertEquals(str(self.geo1.t), str(self.geo2.t))
 
 
 class TestParmWrap(object):
@@ -77,24 +78,24 @@ class TestParmWrap(object):
         self.geo2 = hou.node('/obj').createNode('geo')
 
     def testGetParent(self):
-        assert_equal(-self.geo1.tx, self.geo1)
+        self.assertEquals(-self.geo1.tx, self.geo1)
 
     def testSetParm(self):
         self.geo1.tx = 500
-        assert_equal(self.geo1.tx.eval(), 500)
+        self.assertEquals(self.geo1.tx.eval(), 500)
 
     def testEvalParm(self):
         self.geo1.tx.set(500.0)
-        assert_equal(self.geo1.tx.eval(), 500.0)
+        self.assertEquals(self.geo1.tx.eval(), 500.0)
 
     def testStrParm(self):
         self.geo1.tx.set(500.0)
-        assert_equal(str(self.geo1.tx), str(500.0))
+        self.assertEquals(str(self.geo1.tx), str(500.0))
 
     def testLinkParms(self):
         self.geo1.tx >> self.geo2.tx
         self.geo1.tx = 450.0
-        assert_equal(str(self.geo1.tx), str(self.geo2.tx))
+        self.assertEquals(str(self.geo1.tx), str(self.geo2.tx))
 
 
 
